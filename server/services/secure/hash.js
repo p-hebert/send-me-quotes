92b48a280f010335e877d5ce14af3eb9e8fa1037
p-hebert <philippe.hebert.qc@gmail.com>
2017-01-04 15:49:49 -0500

    GH #5 : Added URL shortener to hash module

--- server/services/secure/hash.js
+++ server/services/secure/hash.js
@@ -3,9 +3,40 @@
 const crypto = require('crypto');
 
 module.exports = {
+  shortener: generate_base_62_hash,
   hash: generate_pwd_hash,
   match: validate_pwd_hash
 };
+
+function generate_base_62_hash(data) {
+  var salt = genSalt(64);
+  var hash = crypto.createHmac('sha256', salt);
+  hash.update(JSON.stringify(data));
+  return hex_to_base62(hash.digest('hex')).substring(0,10);
+}
+
+function hex_to_base62(hexstr){
+  const base62 = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let num = parseInt(hexstr, 16);
+  let b62str = '';
+  let mod = 0;
+  do {
+    num = num / 62;
+    mod = Math.round((num - Math.floor(num))*62);
+    b62str = base62.charAt(mod) + b62str;
+    num = Math.floor(num);
+  }while(num !== 0);
+  return b62str
+}
+
+function base62_to_dec(b62str){
+  const base62 = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let num = 0;
+  for(let i = 0; i < b62str.length; i++){
+    num += Math.pow(62, i)*(base62.indexOf(b62str.charAt(b62str.length-(i+1))));
+  }
+  return num;
+}
 
 function generate_pwd_hash(password) {
   var salt = genSalt(64);
